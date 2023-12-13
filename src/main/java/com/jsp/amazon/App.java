40b4950494a7260b2dea87bc9aaba358bae54018
GokulAnandSJ <gokulanand0110@gmail.com>
2023-12-13 14:23:06 +0530

    change 3 made by gokul

--- src/main/java/com/jsp/amazon/App.java
+++ src/main/java/com/jsp/amazon/App.java
@@ -8,8 +8,8 @@ public class App
 {
 	public static void main( String[] args ){
 		System.out.println( "Hello World!" );
-		//change 1 made by goku
-		// change 2
-		// change 2 made by gok
+		
+		
+		
 	}
 }
